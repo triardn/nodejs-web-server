5f6488bb31581598f7cf96c1d670881f2d47d0f4
Tri Ardini <tr.ardn@gmail.com>
2021-05-30 23:33:20 +0700

    handling http method

--- server.js
+++ server.js
@@ -1,1 +1,46 @@
-console.log('Lets start creating a new web server')
+const http = require('http')
+
+const requestListener = (req, res) => {
+    res.setHeader('Content-Type', 'text/html')
+    res.statusCode = 200
+
+    const { method } = req
+
+    switch (method) {
+        case "GET":
+            res.end('<h1>Method GET</h1>')
+
+            break
+        case "POST":
+            let body = []
+
+            req.on('data', (chunk) => {
+                body.push(chunk)
+            })
+
+            req.on('end', () => {
+                body = Buffer.concat(body).toString()
+                const { name } = JSON.parse(body);
+                res.end(`<h1>Hai, ${name}!</h1>`)
+            })
+
+            break
+        case "PUT":
+            res.end('<h1>Method PUT</h1>')
+
+            break
+        case "DELETE":
+            res.end('<h1>Method DELETE</h1>')
+
+            break
+    }
+}
+
+const server = http.createServer(requestListener)
+
+const port = 5000
+const host = 'localhost'
+
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`)
+})
