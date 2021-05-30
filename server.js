9c94c3e150e14d14d55cf36fcc8d46ed5a6d7a06
Tri Ardini <tr.ardn@gmail.com>
2021-05-30 23:42:54 +0700

    simple routing

--- server.js
+++ server.js
@@ -4,36 +4,68 @@ const requestListener = (req, res) => {
     res.setHeader('Content-Type', 'text/html')
     res.statusCode = 200
 
-    const { method } = req
+    const { method, url } = req
 
-    switch (method) {
-        case "GET":
-            res.end('<h1>Method GET</h1>')
-
+    switch (url) {
+        case '/':
+            if (method == 'GET') {
+                res.end('<h1>Ini adalah homepage</h1>')
+            } else {
+                res.end(`<h1>Halaman ini tidak dapat diakses dengan ${method} request!</h1>`)
+            }
             break
-        case "POST":
-            let body = []
+        case '/about':
+            if (method == 'GET') {
+                res.end('<h1>Ini adalah halaman about</h1>')
+            } else if (method == 'POST') {
+                let body = []
 
-            req.on('data', (chunk) => {
-                body.push(chunk)
-            })
+                req.on('data', (chunk) => {
+                    body.push(chunk)
+                })
 
-            req.on('end', () => {
-                body = Buffer.concat(body).toString()
-                const { name } = JSON.parse(body);
-                res.end(`<h1>Hai, ${name}!</h1>`)
-            })
+                req.on('end', () => {
+                    body = Buffer.concat(body).toString()
+                    const {name} = JSON.parse(body)
 
+                    res.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+                })
+            } else {
+                res.end(`<h1>Halaman ini tidak dapat diakses dengan ${method} request!</h1>`)
+            }
             break
-        case "PUT":
-            res.end('<h1>Method PUT</h1>')
-
-            break
-        case "DELETE":
-            res.end('<h1>Method DELETE</h1>')
-
-            break
+        default:
+            res.end('<h1>Halaman tidak ditemukan</h1>')
     }
+
+    // switch (method) {
+    //     case "GET":
+    //         res.end('<h1>Method GET</h1>')
+
+    //         break
+    //     case "POST":
+    //         let body = []
+
+    //         req.on('data', (chunk) => {
+    //             body.push(chunk)
+    //         })
+
+    //         req.on('end', () => {
+    //             body = Buffer.concat(body).toString()
+    //             const { name } = JSON.parse(body);
+    //             res.end(`<h1>Hai, ${name}!</h1>`)
+    //         })
+
+    //         break
+    //     case "PUT":
+    //         res.end('<h1>Method PUT</h1>')
+
+    //         break
+    //     case "DELETE":
+    //         res.end('<h1>Method DELETE</h1>')
+
+    //         break
+    // }
 }
 
 const server = http.createServer(requestListener)
